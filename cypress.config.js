747a9872626858250d67f7d15b19ebcd415be733
Madelin <madeser1511@gmail.com>
2025-11-11 18:12:27 -0500

    Primer commit: proyecto de pruebas automatizadas con Cypress y CI

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,19 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-       console.log('✅ Mochawesome plugin cargado');
+      console.log(' Mochawesome plugin cargado');
       require('cypress-mochawesome-reporter/plugin')(on);
     },
     specPattern: 'e2e-tests/*.spec.js',
-    supportFile: false // 👈 Desactiva el archivo de soporte
-  },
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    reportDir: 'reports',
-    overwrite: false,
-    html: true,
-    json: true
+    supportFile: 'cypress/support/e2e.js',
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      reportDir: 'reports',
+      overwrite: false,
+      html: false,
+      json: true
+    }
   }
-})
+});
